49e86f9324582fbb30c2421b1211d6e31ae45ba4
virgilius <virgilius.santos@acad.pucrs.br>
2020-01-19 16:45:24 -0300

    create project

---
+++ Podfile
@@ -0,0 +1,19 @@
+source 'https://github.com/virgilius-santos/public-pod-specs.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '12.4'
+
+use_frameworks!
+
+target 'VSMarvelApp' do
+
+  pod "VService"                , :path => '../VSCommonSwiftLibrary'
+  pod "VCore"                   , :path => '../VSCommonSwiftLibrary'
+  pod "VComponents"             , :path => '../VSCommonSwiftLibrary'
+
+  target 'VSMarvelAppTests' do
+    inherit! :search_paths
+
+  end
+
+end
---
+++ VSMarvelApp/Support/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  VSMarvelApp
+//
+//  Created by Virgilius Santos on 19/01/20.
+//  Copyright © 2020 Virgilius Santos. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .red
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+}
+
