318f73601348850871097a097cbb6c84d6635a5a
virgilius <virgilius.santos@acad.pucrs.br>
2020-01-19 16:49:38 -0300

    update lint

--- VSMarvelApp/Support/AppDelegate.swift
+++ VSMarvelApp/Support/AppDelegate.swift
@@ -10,20 +10,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .red
         window?.rootViewController = UIViewController()
         window?.makeKeyAndVisible()
-        
+
         return true
     }
-
-
 }
-
