286d42f63d1e56d726de46118b26bce87b19e704
virgilius <virgilius.santos@acad.pucrs.br>
2020-01-19 21:32:09 -0300

    create enum to api keys

---
+++ VSMarvelApp/Support/Keys.swift
@@ -0,0 +1,17 @@
+//
+//  Keys.swift
+//  VSMarvelApp
+//
+//  Created by Virgilius Santos on 19/01/20.
+//  Copyright © 2020 Virgilius Santos. All rights reserved.
+//
+
+import Foundation
+import Keys
+
+private let keys = VSMarvelAppKeys()
+
+enum ApiKeys {
+    static let marvelApiKey = keys.marvelApiKey
+    static let marvelPrivateKey = keys.marvelPrivateKey
+}
