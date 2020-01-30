52e69467fd96841c89cba6bcf7f761f6ddcfc0b7
virgilius <virgilius.santos@acad.pucrs.br>
2020-01-30 02:18:29 -0300

    fix title

--- VSMarvelApp/Classes/Scene/MainCoordinator.swift
+++ VSMarvelApp/Classes/Scene/MainCoordinator.swift
@@ -13,7 +13,7 @@ final class MainCoordinator {
     }
     
     func start() {
-        let vc = ListViewController(viewModel: ListViewModel(title: "Character", router: self))
+        let vc = ListViewController(viewModel: ListViewModel(title: "Characters", router: self))
         navController?.navigate(to: vc, using: DSNavigationType.push)
     }
 }
@@ -25,7 +25,7 @@ extension MainCoordinator: GridRouter {
     }
     
     func grid_switchToList() {
-        let vc = ListViewController(viewModel: ListViewModel(title: "Character", router: self))
+        let vc = ListViewController(viewModel: ListViewModel(title: "Characters", router: self))
         navController?.navigate(to: vc, using: DSNavigationType.replace)
     }
 }
@@ -37,7 +37,7 @@ extension MainCoordinator: ListRouter {
     }
     
     func list_switchToGrid() {
-        let vc = GridViewController(viewModel: GridViewModel(title: "Character", router: self))
+        let vc = GridViewController(viewModel: GridViewModel(title: "Characters", router: self))
         navController?.navigate(to: vc, using: DSNavigationType.replace)
     }
 }
